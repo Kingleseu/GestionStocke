ec9f7d0b21089000ee04ad2a900e642afc15649a
Kingsleu <ebenndombi99@gmail.com>
2025-12-22 22:10:24 +0100

    Configure for Railway deployment 3

--- redpos/settings.py
+++ redpos/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-CHANGE-THIS-IN-PRODUC
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1''gestionstocke.onrender.com','.onrender.com').split(',')
 
 
 # Application definition
